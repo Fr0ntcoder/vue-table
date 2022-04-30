33ecf20dd3908655f059b62dd016a75721556a06
Fr0ntcoder <jambox228@gmail.com>
2022-04-30 18:13:51 +0800

    actions(async/await)

--- src/store/modules/users/actions.js
+++ src/store/modules/users/actions.js
@@ -1,31 +1,54 @@
 import Users from "@/api/Users";
 
-export const GET_USERS = ({commit,state}) => {
+export const GET_USERS = async ({commit,state}) => {
     const params = {
         page: state.page,
         amount: state.amount
     }
-    Users.all(params).then(response => {
+    try {
+        const response = await Users.all(params);
+        commit("SET_USERS",response.data.results) 
+        localStorage.setItem("users",JSON.stringify(response.data.results))
+    }catch(e) {
+        console.error(e)
+    }
+    /* Users.all(params).then(response => {
         commit("SET_USERS",response.data.results)
         localStorage.setItem("users",JSON.stringify(response.data.results))
-    })
+    }) */
 
 }
 
-export const GET_AMOUNT_USERS = ({commit,state},amount) => {
-    Users.amountUsers(state.page,amount).then(response => {
+export const GET_AMOUNT_USERS = async ({commit,state},amount) => {
+    try {
+        const response = await Users.amountUsers(state.page,amount)
+        commit("SET_USERS",response.data.results) 
+        localStorage.setItem("users",JSON.stringify(response.data.results))
+        commit("SET_AMOUNT_USERS",amount)
+    }catch(e) {
+        console.error(e)
+    }
+    /* Users.amountUsers(state.page,amount).then(response => {
         commit("SET_USERS",response.data.results)
         localStorage.setItem("users",JSON.stringify(response.data.results))
     })
-    commit("SET_AMOUNT_USERS",amount)
+    commit("SET_AMOUNT_USERS",amount) */
 }
 
-export const GET_PAGE_USERS = ({commit,state},page) => {
-    Users.pageUsers(state.amount,page).then(response => {
+export const GET_PAGE_USERS = async ({commit,state},page) => {
+    try {
+        const response = await Users.pageUsers(state.amount,page)
+        commit("SET_USERS",response.data.results)
+        localStorage.setItem("users",JSON.stringify(response.data.results))
+        commit("SET_PAGE_USERS",page)
+    }catch(e) {
+        console.error(e)
+    }
+    /* Users.pageUsers(state.amount,page).then(response => {
         commit("SET_USERS",response.data.results)
         localStorage.setItem("users",JSON.stringify(response.data.results))
     })
-    commit("SET_PAGE_USERS",page)
+    commit("SET_PAGE_USERS",page) */
 }
 
 export const PAGE_NEXT = ({commit}) => {
